e502d6d33fb0d6bfc0e2726da147a9d71a6292da
borbalala <118881249+borbalala@users.noreply.github.com>
2022-11-30 08:42:01 +0100

    Update about.php

--- about.php
+++ about.php
@@ -11,9 +11,10 @@
     <a class="headlink" href="about">About</a>
     <a class="headlink" href="quiz">Quiz</a>
   </header>
-
   <div class="container">
-
+    <h2>About us</h2>
+      <p>We are a team of three students that love music and wanted to make a website gathering lots of songs.</p>
+      <p>Feel free to look up songs or, if you feel like being challenged, try to guess which songs come from whose playlist in the "quiz" tab.</p>
     </div>
 </body>
 </html>
