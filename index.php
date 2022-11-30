179a788724c86b1c2f72d93887809e7e40c2ae55
borbalala <118881249+borbalala@users.noreply.github.com>
2022-11-30 08:31:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
   <header>
     <a class="headlink" href="/">Search music</a>
     <a class="headlink" href="/about">About</a>
-    <a class="headlink" href="/match">Match</a>
     <a class="headlink" href="/quiz">Quiz</a>
   </header>
 
@@ -21,7 +20,7 @@
   <p>Album: <input type="text" name="artist" value=""></p>
   <br>
   <select class="select" name="Country">
-    <option value="0" selected disabled>Select Genre</option>
+    <option value="0" selected disabled>Select Country</option>
     <option value="France">France</option>
     //these options may be filled out by an sql script in the future
 
