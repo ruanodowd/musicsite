5dd7d6ba041f9e60d7025faea0d320ba5279909c
borbalala <118881249+borbalala@users.noreply.github.com>
2022-12-13 11:25:27 +0100

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -3,17 +3,46 @@
 <head>
     <title></title>
     <link rel="stylesheet" href="style.css">
+    <?php
+      $artist = $_GET["Artist"];
+      $song = $_GET["Song"];
+      $album = $_GET["Album"];
+      $servername = "localhost";
+      $username = "root";
+      $password = "";
+      $dbname = "music";
+
+      $con = mysqli_connect($servername, $username, $password, $dbname);
+      if (mysqli_connect_errno()) {
+        die("Connection failed: " . mysqli_connect_errno());
+      }
+    ?>
 </head>
 <body>
   <header>
     <a class="headlink" href="index">Search music</a>
     <a class="headlink" href="about">About</a>
     <a class="headlink" href="quiz">Quiz</a>
+   
   </header>
-  
+ 
   <div class="container">
   <h2>Try to guess whose top 2022 songs this comes from !</h2>
-  
+  <br>  
+   <form class="check" action="check_quiz.php" method="get">
+       
+<?php
+$random = rand(0,1);
+       if ($random=0){
+$sql = "SELECT ida.Artist, ida.Album, ida.Song, ida.Genre FROM ida WHERE ida.ID = rand(1,50);
+$result = mysqli_query($con,$sql);
+foreach($result as $row) {
+  echo $row['title'] ;
+}
+mysqli_close($con);
+?>
+<form>
+
 <input type="radio" name="student" id="Ruan" value="Ruan" />
 <label for="Ruan">Ruan</label>
 <input type="radio" name="student" id="Dominik" value="Dominik" />
@@ -21,8 +50,35 @@
 <input type="radio" name="student" id="Ida" value="Ida" />
 <label for="Ida">Ida</label>
   <br>
-  <button type="submit" name="submit">Check</button>
+  <button type="button" onclick="display()">Check</button
   </form>
-  </div>
+    <br>
+    <div id="disp" style=
+        "color:green; font-size:18px; font-weight:bold;">
+    </div>
+</body>
+    <script>
+        function display() {
+            if(document.getElementById('Ida').checked) {
+                document.getElementById("disp").innerHTML
+                    = document.getElementById("GFG").value
+                    + " is the right answer";
+            }
+            else if(document.getElementById('Ruan').checked) {
+                document.getElementById("disp").innerHTML
+                    = document.getElementById("HTML").value
+                    + " is the wrong answer";  
+            }
+            else if(document.getElementById('Dominik').checked) {
+                document.getElementById("disp").innerHTML
+                    = document.getElementById("JS").value
+                    + " is the wrong answer";  
+            }
+            else {
+                document.getElementById("disp").innerHTML
+                    = "No one selected";
+            }
+    }
+    </script>
 </body>
 </html>
